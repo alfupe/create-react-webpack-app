b16b4f226c4991ad854b004ba9aee2ed1085e0e4
Alex Fuentes <alex@sportmaniacs.com>
2020-07-10 10:17:50 +0200

    added initial src files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './assets/scss/main.scss';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>app is running…</div>
+        );
+    }
+}
