136eb0d3bf96c74c4fd43c956e527b656b953f73
Alex Fuentes <alex@sportmaniacs.com>
2020-10-07 09:59:31 +0200

    prettier y eslint

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import './assets/scss/main.scss';
 
 export default class App extends Component {
-    render() {
-        return (
-            <div>app is running…</div>
-        );
-    }
+  render() {
+    return <div>app is running…</div>;
+  }
 }
