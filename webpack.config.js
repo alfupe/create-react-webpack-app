b59ff1ad67927ba5eee92cdbac23d33f798b3d9f
Alex Fuentes <alex@sportmaniacs.com>
2020-11-09 08:10:02 +0100

    cambio de seteo de environment

--- webpack.config.js
+++ webpack.config.js
@@ -3,115 +3,119 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const isDevelopment = process.env.NODE_ENV === 'development';
 const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  entry: {
-    main: './src/index.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js?version=[hash:8]',
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.(ts|tsx)?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader',
-      },
-      {
-        test: /\.module\.s([ac])ss$/,
-        use: [
-          {
-            loader: isDevelopment
-              ? 'style-loader'
-              : MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              sourceMap: isDevelopment,
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDevelopment,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.s([ac])ss$/,
-        exclude: /\.module.(s([ac])ss)$/,
-        use: [
-          {
-            loader: isDevelopment
-              ? 'style-loader'
-              : MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: 'css-loader',
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDevelopment,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg)$/,
-        loader: 'file-loader',
-      },
-    ],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        test: /\.js(\?.*)?$/i,
-        parallel: true,
-      }),
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.scss'],
-    alias: {
-      components: path.resolve(__dirname, 'src/components'),
+module.exports = (env) => {
+  const isDevelopment = env === 'development';
+  const isProduction = env === 'production';
+
+  return {
+    entry: {
+      main: './src/index.js',
     },
-  },
-  devServer: {
-    hot: true,
-    publicPath: '/',
-    historyApiFallback: true,
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './public/index.html',
-      filename: './index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: isDevelopment ? '[name].css' : '[name].css?version=[hash:8]',
-      chunkFilename: isDevelopment ? '[id].css' : '[id].css?version=[hash:8]',
-    }),
-    new CopyPlugin({
-      patterns: [{ from: './public' }],
-    }),
-    new CleanWebpackPlugin(),
-  ],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js?version=[hash:8]',
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.(ts|tsx)?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.html$/,
+          loader: 'html-loader',
+        },
+        {
+          test: /\.module\.s([ac])ss$/,
+          use: [
+            {
+              loader: isDevelopment
+                ? 'style-loader'
+                : MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                sourceMap: isDevelopment,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.s([ac])ss$/,
+          exclude: /\.module.(s([ac])ss)$/,
+          use: [
+            {
+              loader: isDevelopment
+                ? 'style-loader'
+                : MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg)$/,
+          loader: 'file-loader',
+        },
+      ],
+    },
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new TerserPlugin({
+          test: /\.js(\?.*)?$/i,
+          parallel: true,
+        }),
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.scss'],
+      alias: {
+        components: path.resolve(__dirname, 'src/components'),
+      },
+    },
+    devServer: {
+      hot: true,
+      publicPath: '/',
+      historyApiFallback: true,
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './public/index.html',
+        filename: './index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: isDevelopment ? '[name].css' : '[name].css?version=[hash:8]',
+        chunkFilename: isDevelopment ? '[id].css' : '[id].css?version=[hash:8]',
+      }),
+      new CopyPlugin({
+        patterns: [{ from: './public' }],
+      }),
+      new CleanWebpackPlugin(),
+    ],
+  };
 };
